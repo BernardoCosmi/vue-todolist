5d532597017d195013b7ef841dd52d221a8ab577
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-01-29 17:14:14 +0100

    add: display of the array in the console log and minor bug fix

--- assets/main.js
+++ assets/main.js
@@ -6,19 +6,18 @@ createApp({
             tasks: [
                 {
                     text: "Fare la spesa",
-                    done: false 
-                },{
-                    text: "Studiare per l'esame",
                     done: true 
                 },{
-                    text: "Pulire la casa",
+                    text: "Studiare per l'esame",
                     done: false 
                 },{
-                    text: "Andare in palestra",
+                    text: "Trovare la voglia di uscire di casa",
+                    done: false 
+                },{
+                    text: "Giocare a Rainbow Six Siege",
                     done: true 
                 }
             ],
-
             newTask:" ",
         };
     },
@@ -26,15 +25,20 @@ createApp({
             addTask(){
                 if (this.newTask.trim() !== ""){
                     this.tasks.push({text: this.newTask, done: false })
-                    this.newTodo = "";
+                    this.tasks = [...this.tasks];
+                    this.newTask = "";
                 }
             },
             removeTask(index){
                 this.tasks.splice(index, 1);
+                this.tasks = [...this.tasks];
             },
-            completeTask(){
-
+            completeTask(index){
+                this.tasks[index].done = !this.tasks[index].done;
             }
+        },
+        mounted() {
+            console.log(this.tasks);  // Stampa l'array completo in console al momento del montaggio (mount)
         },
 }).mount('#app');
   
